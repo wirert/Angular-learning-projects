669c136a357b55f1bc3f7e92bd21f81fa36d8fd9
Krasimir Nikolov <wirert@gmail.com>
2024-01-29 19:00:38 +0200

    Introduction to Angular course by Mark Thompson app project

---
+++ homes-app/src/app/home/home.component.ts
@@ -0,0 +1,54 @@
+import { Component, inject } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HousingLocationComponent } from "../housing-location/housing-location.component";
+import { HousingLocation } from "../housing-location";
+import { HousingService } from "../housing.service";
+
+@Component({
+  selector: "app-home",
+  standalone: true,
+  imports: [CommonModule, HousingLocationComponent],
+  template: `
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city" #filter />
+        <button
+          class="primary"
+          type="button"
+          (click)="filterResults(filter.value)"
+        >
+          Search
+        </button>
+      </form>
+    </section>
+    <section class="results">
+      <app-housing-location
+        *ngFor="let housingLocation of filteredLocationList"
+        [housingLocation]="housingLocation"
+      ></app-housing-location>
+    </section>
+  `,
+  styleUrls: ["./home.component.css"],
+})
+export class HomeComponent {
+  housingLocationList: HousingLocation[] = [];
+  filteredLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+
+  constructor() {
+    this.housingService
+      .getAllHousinLocations()
+      .then((housingLocationList: HousingLocation[]) => {
+        this.housingLocationList = housingLocationList;
+        this.filteredLocationList = housingLocationList.slice();
+      });
+  }
+
+  filterResults(filterText: string) {
+    this.filteredLocationList = !filterText
+      ? this.housingLocationList
+      : this.housingLocationList.filter((location) =>
+          location?.city.toLowerCase().includes(filterText.toLowerCase())
+        );
+  }
+}
