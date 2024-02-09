8a5e4a40fe3156f09a2cbd2f5e52e8f01f618c42
Krasimir Nikolov <wirert@gmail.com>
2024-02-09 16:57:12 +0200

    use Dinamic component with ComponentFactoryResolver

--- dinamic-components/src/app/app.module.ts
+++ dinamic-components/src/app/app.module.ts
@@ -1,23 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { UsersComponent } from './users/users.component';
-import { ConfirmDeleteComponent } from './users/confirm-delete/confirm-delete.component';
-import { RoutingModule } from './routing.module';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { UsersComponent } from "./users/users.component";
+import { ConfirmDeleteComponent } from "./users/confirm-delete/confirm-delete.component";
+import { RoutingModule } from "./routing.module";
+import { ViewContainer } from "./viewContainer.directive";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     UsersComponent,
-    ConfirmDeleteComponent
+    ConfirmDeleteComponent,
+    ViewContainer,
   ],
-  imports: [
-    BrowserModule,
-    RoutingModule
-  ],
+  imports: [BrowserModule, RoutingModule],
   providers: [],
   bootstrap: [AppComponent],
+  entryComponents: [ConfirmDeleteComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- dinamic-components/src/app/users/users.component.ts
+++ dinamic-components/src/app/users/users.component.ts
@@ -1,6 +1,14 @@
-import { Component, OnInit } from "@angular/core";
+import {
+  Component,
+  ComponentFactoryResolver,
+  OnInit,
+  ViewChild,
+} from "@angular/core";
 import { User } from "../Models/User";
 import { UserService } from "../Services/user.service";
+import { ConfirmDeleteComponent } from "./confirm-delete/confirm-delete.component";
+import { ViewContainer } from "../viewContainer.directive";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-users",
@@ -8,19 +16,28 @@ import { UserService } from "../Services/user.service";
   styleUrls: ["./users.component.css"],
 })
 export class UsersComponent implements OnInit {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private compFactResolver: ComponentFactoryResolver
+  ) {}
 
   users: User[] = [];
   showConfirmDeleteComp = false;
   userToDelete: User;
+
+  @ViewChild(ViewContainer, { static: false }) container: ViewContainer;
+
+  onConfirmObs: Subscription;
 
   ngOnInit() {
     this.users = this.userService.users;
   }
 
   OnDeleteClicked(user: User) {
-    this.showConfirmDeleteComp = true;
+    // this.showConfirmDeleteComp = true;
     this.userToDelete = user;
+
+    this.showConfirmDelete(this.userToDelete);
   }
 
   OnUserDeleteConfirmation(deleteUser: boolean) {
@@ -29,5 +46,31 @@ export class UsersComponent implements OnInit {
       const userIndex = this.userService.users.indexOf(this.userToDelete);
       this.userService.users.splice(userIndex, 1);
     }
+  }
+
+  showConfirmDelete(user: User) {
+    const confirmDelCompFactory = this.compFactResolver.resolveComponentFactory(
+      ConfirmDeleteComponent
+    );
+
+    const containerViewRef = this.container.viewContainer;
+    containerViewRef.clear();
+
+    const componentRef = containerViewRef.createComponent(
+      confirmDelCompFactory
+    );
+
+    componentRef.instance.userToDelete = user;
+    this.onConfirmObs = componentRef.instance.OnConfirmation.subscribe(
+      (deleteUser: boolean) => {
+        this.onConfirmObs.unsubscribe();
+        containerViewRef.clear();
+
+        if (deleteUser) {
+          const userIndex = this.userService.users.indexOf(this.userToDelete);
+          this.userService.users.splice(userIndex, 1);
+        }
+      }
+    );
   }
 }
