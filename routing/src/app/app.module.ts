35756e814cb5ddef7dd851ba49325734dc191153
Krasimir Nikolov <wirert@gmail.com>
2024-02-09 20:12:31 +0200

    routes added

--- routing/src/app/app.module.ts
+++ routing/src/app/app.module.ts
@@ -1,29 +1,37 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
-import { ServicesService } from './Services/services.service';
-import { HomeComponent } from './home/home.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
-import { ContactComponent } from './contact/contact.component';
-import { AboutComponent } from './about/about.component';
-import { BannerComponent } from './home/banner/banner.component';
-import { ServicesComponent } from './home/services/services.component';
-import { TestimonyComponent } from './home/testimony/testimony.component';
-import { ContactUsComponent } from './home/contact-us/contact-us.component';
-import { PopularComponent } from './home/popular/popular.component';
-import { CourseService } from './Services/course.service';
-import { CoursesComponent } from './courses/courses.component';
-import { CourseDetailComponent } from './courses/course-detail/course-detail.component';
-import { LoginComponent } from './login/login.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { CheckoutComponent } from './checkout/checkout.component';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { Routes, RouterModule, UrlSerializer } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { ServicesService } from "./Services/services.service";
+import { HomeComponent } from "./home/home.component";
+import { FooterComponent } from "./footer/footer.component";
+import { HeaderComponent } from "./header/header.component";
+import { ContactComponent } from "./contact/contact.component";
+import { AboutComponent } from "./about/about.component";
+import { BannerComponent } from "./home/banner/banner.component";
+import { ServicesComponent } from "./home/services/services.component";
+import { TestimonyComponent } from "./home/testimony/testimony.component";
+import { ContactUsComponent } from "./home/contact-us/contact-us.component";
+import { PopularComponent } from "./home/popular/popular.component";
+import { CourseService } from "./Services/course.service";
+import { CoursesComponent } from "./courses/courses.component";
+import { CourseDetailComponent } from "./courses/course-detail/course-detail.component";
+import { LoginComponent } from "./login/login.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
+import { CheckoutComponent } from "./checkout/checkout.component";
+import { FormsModule } from "@angular/forms";
+import { LowerCaseUrlSerializer } from "./lowerCaseUrlSerializer";
 
-
-
-
+const routes: Routes = [
+  { path: "", component: HomeComponent, title: "Home page" },
+  //or
+  // { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "home", component: HomeComponent, title: "Home page" },
+  { path: "about", component: AboutComponent },
+  { path: "contact", component: ContactComponent },
+  { path: "courses", component: CoursesComponent },
+  { path: "**", component: NotFoundComponent },
+];
 
 @NgModule({
   declarations: [
@@ -42,13 +50,17 @@ import { FormsModule } from '@angular/forms';
     CourseDetailComponent,
     LoginComponent,
     NotFoundComponent,
-    CheckoutComponent
+    CheckoutComponent,
   ],
-  imports: [
-    BrowserModule,
-    FormsModule
+  imports: [RouterModule.forRoot(routes), BrowserModule, FormsModule],
+  providers: [
+    ServicesService,
+    CourseService,
+    {
+      provide: UrlSerializer,
+      useClass: LowerCaseUrlSerializer,
+    },
   ],
-  providers: [ServicesService, CourseService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
