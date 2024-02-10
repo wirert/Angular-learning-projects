6c916d31f8d9e0b5595d4ce50b357edf428010e6
Krasimir Nikolov <wirert@gmail.com>
2024-02-10 17:57:15 +0200

    home banner search courses implemented

--- routing/src/app/courses/courses.component.ts
+++ routing/src/app/courses/courses.component.ts
@@ -1,13 +1,37 @@
-import { Component, inject } from "@angular/core";
+import { Component, OnDestroy, OnInit, inject } from "@angular/core";
 import { Course } from "../Models/course";
 import { CourseService } from "../Services/course.service";
+import { ActivatedRoute } from "@angular/router";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-courses",
   templateUrl: "./courses.component.html",
   styleUrls: ["./courses.component.css"],
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit, OnDestroy {
   coursesService = inject(CourseService);
-  AllCourses: Course[] = this.coursesService.courses;
+  AllCourses: Course[];
+
+  activeRoute: ActivatedRoute = inject(ActivatedRoute);
+  routeObs: Subscription;
+  searchString: string;
+
+  ngOnInit(): void {
+    this.routeObs = this.activeRoute.queryParamMap.subscribe((data) => {
+      this.searchString = data.get("search");
+
+      if (!this.searchString) {
+        this.AllCourses = this.coursesService.courses;
+      } else {
+        this.AllCourses = this.coursesService.courses.filter((c) =>
+          c.title.toLowerCase().includes(this.searchString.toLowerCase())
+        );
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.routeObs.unsubscribe();
+  }
 }
--- routing/src/app/home/banner/banner.component.ts
+++ routing/src/app/home/banner/banner.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-banner',
-  templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.css']
+  selector: "app-banner",
+  templateUrl: "./banner.component.html",
+  styleUrls: ["./banner.component.css"],
 })
 export class BannerComponent {
+  router: Router = inject(Router);
 
+  OnBtnClick(value: string) {
+    this.router.navigate(["/courses"], { queryParams: { search: value } });
+  }
 }
