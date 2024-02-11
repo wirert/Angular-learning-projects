9f554c6aebb93f7da9c99c3b24039a52e7c631e5
Krasimir Nikolov <wirert@gmail.com>
2024-02-11 19:57:45 +0200

    show loading animation while load data using navigation events

--- routing/src/app/app.component.ts
+++ routing/src/app/app.component.ts
@@ -1,11 +1,35 @@
-import { Component, inject } from '@angular/core';
-
+import { Component, OnInit, inject } from "@angular/core";
+import {
+  Router,
+  Event,
+  NavigationStart,
+  NavigationEnd,
+  NavigationCancel,
+  NavigationError,
+} from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  title = 'Angular Routing';
+export class AppComponent implements OnInit {
+  title = "Angular Routing";
+  router: Router = inject(Router);
+
+  showLoader: boolean = false;
+
+  ngOnInit(): void {
+    this.router.events.subscribe((routerEvent: Event) => {
+      if (routerEvent instanceof NavigationStart) {
+        this.showLoader = true;
+      } else if (
+        routerEvent instanceof NavigationEnd ||
+        routerEvent instanceof NavigationCancel ||
+        routerEvent instanceof NavigationError
+      ) {
+        this.showLoader = false;
+      }
+    });
+  }
 }
