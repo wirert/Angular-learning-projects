64d114eb0bfdbf28cd7d6886b781db9ac889799b
Krasimir Nikolov <wirert@gmail.com>
2024-02-11 11:11:32 +0200

    jump to fragment in home component implemented

--- routing/src/app/home/home.component.ts
+++ routing/src/app/home/home.component.ts
@@ -1,8 +1,27 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit, inject } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.css"],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit, OnDestroy {
+  activeRoute: ActivatedRoute = inject(ActivatedRoute);
+  fragmentObs: Subscription;
+
+  ngOnInit(): void {
+    this.fragmentObs = this.activeRoute.fragment.subscribe((data) => {
+      this.JumpToSection(data);
+    });
+  }
+
+  JumpToSection(section: string) {
+    document.getElementById(section).scrollIntoView({ behavior: "smooth" });
+  }
+
+  ngOnDestroy(): void {
+    this.fragmentObs.unsubscribe();
+  }
+}
