c12f2d8ef24361a794795aa01346ba82b7ad3759
Krasimir Nikolov <wirert@gmail.com>
2024-02-13 13:15:22 +0200

    added  validations for names and email

--- template-driven-form/src/app/app.component.ts
+++ template-driven-form/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'template-driven-form';
+  title = "template-driven-form";
+  @ViewChild("regForm") regForm: NgForm;
+
+  genders = [
+    { id: "check-male", value: "male", display: "Male" },
+    { id: "check-female", value: "female", display: "Female" },
+    { id: "check-other", value: "other", display: "Prefer not to say" },
+  ];
+
+  OnFormSubmitted() {
+    console.log(this.regForm);
+  }
 }
