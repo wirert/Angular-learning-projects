8c4690b83d71f1918ab895d6ffa346591d3dab8d
Krasimir Nikolov <wirert@gmail.com>
2024-02-16 11:45:24 +0200

    Auth Interceptor added

---
+++ angulat-http-client/src/app/Services/auth-interceptor.service.ts
@@ -0,0 +1,19 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from "@angular/common/http";
+import { Observable } from "rxjs";
+
+export class AuthInterceptorService implements HttpInterceptor {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const modifiedReq = req.clone({
+      headers: req.headers.append("auth", "asdf23"),
+    });
+    return next.handle(modifiedReq);
+  }
+}
--- angulat-http-client/src/app/app.module.ts
+++ angulat-http-client/src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { CreateTaskComponent } from './dashboard/create-task/create-task.component';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { TaskDetailsComponent } from './dashboard/task-details/task-details.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { CreateTaskComponent } from "./dashboard/create-task/create-task.component";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { TaskDetailsComponent } from "./dashboard/task-details/task-details.component";
+import { AuthInterceptorService } from "./Services/auth-interceptor.service";
 
 @NgModule({
   declarations: [
@@ -17,14 +18,16 @@ import { TaskDetailsComponent } from './dashboard/task-details/task-details.comp
     FooterComponent,
     DashboardComponent,
     CreateTaskComponent,
-    TaskDetailsComponent
+    TaskDetailsComponent,
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true,
+    },
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
